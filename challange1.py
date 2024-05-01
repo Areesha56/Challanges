00571371fdacd22b865d0fe6639a3ae22359c135
Areesha56 <80579991+Areesha56@users.noreply.github.com>
2024-05-01 11:17:35 +0100

    Hackerank challanges 30days of code

---
+++ challange1.py
@@ -0,0 +1,3 @@
+print('Hello, world.')
+input_string=input('Welcome to 30 Days of Code!')
+print( input_string )
---
+++ challange2.py
@@ -0,0 +1,17 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+int_var=input()
+doub_var=input()
+str_var=input()
+# Read and save an integer, double, and String to your variables.
+sum=i+int(int_var)
+sum_doub=d+float(doub_var)
+# Print the sum of both integer variables on a new line.
+print(sum)
+# Print the sum of the double variables on a new line.
+print(sum_doub)
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+print(s+str_var)
